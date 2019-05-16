ffcebb02110d35e69a40b20c9a5c3de74b0090e9
github username <github email address>
2019-05-16 18:34:40 +0100

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
-end
+
+   attr_reader :name
+   attr_accessor :mood
+   attr_writer
+   
+   def initialize(name, mood="nervous")
+     @name = name
+     @mood = mood
+   end
+   
+   
+   end
+
+# Developed in partnership with amyequinn: https://github.com/amyequinn
