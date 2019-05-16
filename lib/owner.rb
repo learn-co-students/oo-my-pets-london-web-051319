dc0feeda2c841a364fa6daebb6c21af7154ac9b5
flexkat <catherinemaritz1@gmail.com>
2019-05-16 15:46:45 +0100

    buying pets instance methods

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,14 @@
 class Owner
   # code goes here
   @@all_owners = []
+  attr_reader :species
+  attr_accessor :name, :pets
 
   def initialize(name)
     @@all_owners << self
+    @species = "human"
+    @name = name
+    @pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
   def self.all
@@ -16,4 +21,20 @@ class Owner
 
   def reset_all
   end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+ def buy_fish(name)
+  self.pets[:fishes] << Fish.new(name)
+ end
+
+ def buy_cat(name)
+  self.pets[:cats] << Cat.new(name)
+ end
+
+ def buy_dog(name)
+  self.pets[:dogs] << Dog.new(name)
+ end
 end
