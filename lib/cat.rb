827a64580d0d083b381e188d746c0c3e3fdefd1d
Pedro Loureiro <pedro.nunes.loureiro@gmail.com>
2019-05-16 16:50:27 +0100

    Finished, all working with spec

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,14 @@
 class Cat
   # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,15 @@
 class Fish
   # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
 end
