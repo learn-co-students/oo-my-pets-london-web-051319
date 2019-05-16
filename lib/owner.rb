e7c1a6458eee9f589efe146daab2e7d49abfbaeb
flexkat <catherinemaritz1@gmail.com>
2019-05-16 16:05:47 +0100

    my pets methods collabing with aaron

--- lib/owner.rb
+++ lib/owner.rb
@@ -19,6 +19,10 @@ class Owner
     @@all_owners.count
   end
 
+  def self.reset_all
+    @@all_owners.clear
+  end
+
   def reset_all
   end
 
@@ -26,15 +30,47 @@ class Owner
     "I am a #{self.species}."
   end
 
- def buy_fish(name)
-  self.pets[:fishes] << Fish.new(name)
- end
+  def buy_fish(name)
+    self.pets[:fishes] << Fish.new(name)
+  end
 
- def buy_cat(name)
-  self.pets[:cats] << Cat.new(name)
- end
+  def buy_cat(name)
+    self.pets[:cats] << Cat.new(name)
+  end
 
- def buy_dog(name)
-  self.pets[:dogs] << Dog.new(name)
- end
+  def buy_dog(name)
+    self.pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    self.pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end 
+  end
+
+  def play_with_cats
+    self.pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    self.pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    self.pets.each do |key, value|
+      value.each do |animal|
+        animal.mood = "nervous"
+      end
+      self.pets[key].clear
+    end
+  end
+
+  def list_pets
+    return "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
+  end 
+
 end
