c5696720976458d91ab300192c54cc5b13b9dbe1
Alex Johnston <alexjohnston@ymail.com>
2019-05-16 15:41:25 +0100

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,28 @@
 class Cat
-  # code goes here
+
+  @@all = []
+
+attr_reader :name
+attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_cat
+    @@all << self
+  end
+
+  def playtime
+    @mood = "happy"
+  end
+
+  def sell
+    @mood = "nervous"
+  end
 end
