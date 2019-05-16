15364d9d18496902698539851ba0f842dcf1f787
Aaron Summers <aaron.summers2797@gmail.com>
2019-05-16 15:32:33 +0100

    first 2 class methods work

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,19 @@
 class Owner
   # code goes here
+  @@all_owners = []
+
+  def initialize(name)
+    @@all_owners << self
+  end
+
+  def self.all
+    @@all_owners
+  end
+
+  def self.count
+    @@all_owners.count
+  end
+
+  def reset_all
+  end
 end
